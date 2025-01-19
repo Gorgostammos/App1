956dea6fd992674cb55e58020372995327c96d0e
Gorgostammos <gorgos2002@icloud.com>
2025-01-19 15:03:00 +0100

    type

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,3 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+// referency Type
 string myname = "Sai";
+
+// Value Type
+int myage = 25;
+
+double myheight = 5.7;
+double pi = 3.14;
+
+byte mybyte = 255;
+
+long mylong = 1234567890;
+
+short myshort = 12345;
+
+float myfloat = 123.456f;
+
